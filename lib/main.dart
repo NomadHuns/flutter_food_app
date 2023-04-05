5202a1afef038c06b3acb2e1f9c855cc4cd702d4
kimalss22 <kimalss22@gmail.com>
2023-04-05 12:28:25 +0900

    타이틀 및 네이게이션 바 생성

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,10 @@ class MyApp extends StatelessWidget {
           backgroundColor: kWhiteColor,
           elevation: 0,
         ),
+        textTheme: const TextTheme(
+          headlineSmall: TextStyle(fontWeight: FontWeight.bold),
+          labelLarge: TextStyle(fontWeight: FontWeight.bold),
+        ),
       ),
       home: const HomePage(),
     );
@@ -37,11 +41,57 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         actions: [
-          SvgPicture.asset('assets/icons/menu.svg', height: 11,),
-          SizedBox(width: 20),
+          SvgPicture.asset(
+            'assets/icons/menu.svg',
+            height: 11,
+          ),
+          const SizedBox(width: 20),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              'Simple way to find \n'
+              'Tasty food',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+          ),
+          const SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                CategoryTile(title: 'all', active: true),
+                CategoryTile(title: 'Italian'),
+                CategoryTile(title: 'Asian'),
+                CategoryTile(title: 'Chinese'),
+                CategoryTile(title: 'Burgers'),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
 }
 
+class CategoryTile extends StatelessWidget {
+  const CategoryTile({Key? key, required this.title, this.active = false}) : super(key: key);
+
+  final String title;
+  final bool active;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 30),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.labelLarge?.copyWith(color: active ? kPrimaryColor : kTextColor.withOpacity(0.4)),
+      ),
+    );
+  }
+}
